ba51e1c01991de2cf5cc706bb58f7e14bfb89421
Paulo Ventura <noubarijou@live.com>
2022-10-27 10:48:27 -0300

    exercises 1.1 and 1.2 for part1 intro to react

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
+  return (
+    <div>
+      {" "}
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+
+    </div>
+  );
+};
+
+export default Content;
